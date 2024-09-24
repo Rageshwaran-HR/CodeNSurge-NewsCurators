89391d1a8a7cde89fcd4ff7a3450ce2ce7366e04
Leeeroy <90497821+Shadow-Knight503@users.noreply.github.com>
2024-09-24 14:31:57 +0530

    Added Preferences

--- newscurator/src/App.js
+++ newscurator/src/App.js
@@ -1,9 +1,9 @@
-import React, { useEffect } from "react";
+import React, {useEffect, useState} from "react";
 import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useParams,
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    useParams,
 } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer"; // Ensure this matches exactly
@@ -11,91 +11,103 @@ import "./Styles/Layout.css"; // Ensure this includes the styles for the floatin
 import WeatherDetails from "./components/WeatherData";
 import MarketChartWidget from "./components/stockmarket";
 import Carousel from "./components/Carousel";
+import Cookies from "universal-cookie";
+import Pref from "./components/Pref";
 
 const CategoryPage = () => {
-  const { category } = useParams();
-  return (
-    <>
-      <Carousel />
-      <div
-        id="ww_c289b208976c5"
-        v="1.3"
-        loc="auto"
-        a='{"t":"ticker","lang":"en","sl_lpl":1,"ids":[],"font":"Times","sl_ics":"one_a","sl_sot":"celsius","cl_bkg":"image","cl_font":"#FFFFFF","cl_cloud":"#FFFFFF","cl_persp":"#81D4FA","cl_sun":"#FFC107","cl_moon":"#FFC107","cl_thund":"#FF5722"}'
-      >
-        <a
-          href="https://weatherwidget.org/"
-          id="ww_c289b208976c5_u"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Weather widget html
-        </a>
-      </div>
-      <script
-        async
-        src="https://app3.weatherwidget.org/js/?id=ww_c289b208976c5"
-      ></script>
-    </>
-  );
+    const {category} = useParams();
+    return (
+        <>
+            <Carousel/>
+            <div
+                id="ww_c289b208976c5"
+                v="1.3"
+                loc="auto"
+                a='{"t":"ticker","lang":"en","sl_lpl":1,"ids":[],"font":"Times","sl_ics":"one_a","sl_sot":"celsius","cl_bkg":"image","cl_font":"#FFFFFF","cl_cloud":"#FFFFFF","cl_persp":"#81D4FA","cl_sun":"#FFC107","cl_moon":"#FFC107","cl_thund":"#FF5722"}'
+            >
+                <a
+                    href="https://weatherwidget.org/"
+                    id="ww_c289b208976c5_u"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Weather widget html
+                </a>
+            </div>
+            <script
+                async
+                src="https://app3.weatherwidget.org/js/?id=ww_c289b208976c5"
+            ></script>
+        </>
+    );
 };
 
 function App() {
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://app3.weatherwidget.org/js/?id=ww_c289b208976c5";
-    script.async = true;
-    document.body.appendChild(script);
+    const cookie = new Cookies
+    const [isPrf, setPrf] = useState(cookie.get('Pref'))
+    const nav = ""
 
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
 
-  return (
-    <Router>
-      <div className="app-container">
-        <Navbar />
-        <main className="main-content">
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <>
-                  <Carousel />
-                  <div
-                    style={{ "margin-bottom": "200px" }}
-                    id="ww_c289b208976c5"
-                    v="1.3"
-                    loc="auto"
-                    a='{"t":"ticker","lang":"en","sl_lpl":1,"ids":[],"font":"Times","sl_ics":"one_a","sl_sot":"celsius","cl_bkg":"image","cl_font":"#FFFFFF","cl_cloud":"#FFFFFF","cl_persp":"#81D4FA","cl_sun":"#FFC107","cl_moon":"#FFC107","cl_thund":"#FF5722"}'
-                  >
-                    <a
-                      href="https://weatherwidget.org/"
-                      id="ww_c289b208976c5_u"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      Weather widget html
-                    </a>
-                  </div>
-                  <div className="layout-container"></div>
-                </>
-              }
-            />
-          </Routes>
-        </main>
-        <WeatherDetails />
-        <div class="finlogix-containers">
-          <div class="finlogix-container"></div>
-        </div>
-        <div hidden>
-          <MarketChartWidget />
-        </div>
-        <Footer />
-      </div>
-    </Router>
-  );
+
+    useEffect(() => {
+        const script = document.createElement("script");
+        script.src = "https://app3.weatherwidget.org/js/?id=ww_c289b208976c5";
+        script.async = true;
+        document.body.appendChild(script);
+
+        return () => {
+            document.body.removeChild(script);
+        };
+    }, []);
+
+    return (
+        <Router>
+            <div className="app-container">
+                <Navbar nav={nav}/>
+                <main className="main-content relative">
+                    {!isPrf ? (
+                        // Registeration page
+                        <Pref setPrf={setPrf} />
+                    ) : (<div></div>)}
+                    <Routes>
+                        <Route
+                            path="/"
+                            element={
+                                <>
+                                    <Carousel/>
+                                    <div
+                                        style={{"margin-bottom": "200px"}}
+                                        id="ww_c289b208976c5"
+                                        v="1.3"
+                                        loc="auto"
+                                        a='{"t":"ticker","lang":"en","sl_lpl":1,"ids":[],"font":"Times","sl_ics":"one_a","sl_sot":"celsius","cl_bkg":"image","cl_font":"#FFFFFF","cl_cloud":"#FFFFFF","cl_persp":"#81D4FA","cl_sun":"#FFC107","cl_moon":"#FFC107","cl_thund":"#FF5722"}'
+                                    >
+                                        <a
+                                            href="https://weatherwidget.org/"
+                                            id="ww_c289b208976c5_u"
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                        >
+                                            Weather widget html
+                                        </a>
+                                    </div>
+                                    <div className="layout-container"></div>
+                                </>
+                            }
+                        />
+                    </Routes>
+                </main>
+                <WeatherDetails/>
+                <div class="finlogix-containers">
+                    <div class="finlogix-container"></div>
+                </div>
+                <div hidden>
+                    <MarketChartWidget/>
+                </div>
+                <Footer/>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
--- newscurator/src/components/Navbar.jsx
+++ newscurator/src/components/Navbar.jsx
@@ -1,49 +1,64 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom"; // Import Link for navigation
+import React, {useEffect, useState} from "react";
+import {Link} from "react-router-dom"; // Import Link for navigation
 import "../Styles/navbar.css";
 
-const Navbar = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+const Navbar = (props) => {
+    const [nav, setNav] = useState(["World", "Politics", "Business", "Technology", "Sports",
+        "Entertainment", "Health", "Science", "Opinion"])
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
+    function parseLst() {
+        let nvl = props.nav
+        if(nvl === "") {
+            setNav(["World", "Politics", "Business", "Technology", "Sports",
+        "Entertainment", "Health", "Science", "Opinion"])
+        } else {
+            console.log(nvl)
+            let val = nvl.split("%2C")
+            let nvs = []
+            // eslint-disable-next-line array-callback-return
+            val.map((ind, nvd) => {
+                nvs[ind] = nvd
+                val.remove(nvd)
+            })
+            nvs.concat(val)
+            setNav(nvs)
+        }
+    }
 
-  return (
-    <header className="header">
-      <img
-        src="./Untitled.png"
-        className="header-logo"
-        alt="News Logo"
-      />
-      <div className={`hamburger ${isMenuOpen ? "active" : ""}`} onClick={toggleMenu}>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-      <nav className={`menu ${isMenuOpen ? "active" : ""}`}>
-        <ul className="menu-list">
-          {[
-            "World",
-            "Politics",
-            "Business",
-            "Technology",
-            "Sports",
-            "Entertainment",
-            "Health",
-            "Science",
-            "Opinion"
-          ].map((item) => (
-            <li className="menu-list-item" key={item}>
-                <Link className="menu-link" to={`/${item}`}>
-                {item}
-                </Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
-  );
+    useEffect(() => {
+        parseLst()
+    }, []);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    return (
+        <header className="header">
+            <img
+                src="./Untitled.png"
+                className="header-logo"
+                alt="News Logo"
+            />
+            <div className={`hamburger ${isMenuOpen ? "active" : ""}`} onClick={toggleMenu}>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <nav className={`menu ${isMenuOpen ? "active" : ""}`}>
+                <ul className="menu-list">
+                    {nav.map((item) => (
+                        <li className="menu-list-item" key={item}>
+                            <Link className="menu-link" to={`/${item}`}>
+                                {item}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </header>
+    );
 };
 
 export default Navbar;
